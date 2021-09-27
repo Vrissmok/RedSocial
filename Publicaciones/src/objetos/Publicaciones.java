d7f7734ab0be279081de59446ad9f95ccd25578e
Vrissmok <Cristian.cmma56@gmail.com>
2021-09-26 23:22:47 -0500

    Update Publicaciones.java
    Creación de variables y la asignación de nombre del usuario

--- Publicaciones/src/objetos/Publicaciones.java
+++ Publicaciones/src/objetos/Publicaciones.java
@@ -5,10 +5,67 @@
  */
 package objetos;
 
+import objetos.Usuario;
+
 /**
  *
  * @author crist
  */
-public class Publicaciones {
+public class Publicaciones{
+    
+    /**
+    *   Contenido de la publicación.
+    */
+    private String contenidoPublicación;
+    
+    /**
+    *   Nombre de quieén la publico.
+    */
+    private String nombrePublico;
+    
+    /**
+    *   Si es publico o para amigos.
+    */
+    private String pubOamigos;
+
+    /**
+     * 
+     * @param contenidoPublicación
+     * @param nombrePublico
+     * @param pubOamigos 
+     * 
+     */
+    
+    public Publicaciones(String contenidoPublicación, String pubOamigos, Usuario usuario) {
+        this.contenidoPublicación = contenidoPublicación;
+        nombrePublico = usuario.getNombre();
+        this.pubOamigos = pubOamigos;
+    }
+
+    public String getContenidoPublicación() {
+        return contenidoPublicación;
+    }
+
+    public String getNombrePublico() {
+        return nombrePublico;
+    }
+
+    public String getPubOamigos() {
+        return pubOamigos;
+    }
+
+    public void setContenidoPublicación(String contenidoPublicación) {
+        this.contenidoPublicación = contenidoPublicación;
+    }
+
+    public void setNombrePublico(String nombrePublico) {
+        this.nombrePublico = nombrePublico;
+    }
+
+    public void setPubOamigos(String pubOamigos) {
+        this.pubOamigos = pubOamigos;
+    }
+    
+    
     
 }
